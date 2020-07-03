60890207d684aa7ba60c5d0a4a3ef65139f73fd6
Thomas Turner <thomasturner@pop-os.localdomain>
2020-07-03 16:40:50 +0100

    updated file to main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# Work with Python 3.6
 import discord
 
 TOKEN = 'NzI3OTk2MTkwMDkxNzcyMDY3.Xvz-gQ.q6hxill_x1G_GvzF_ePo9F3jfig'
 
 client = discord.Client()
+
+botReady = False
 
 @client.event
 async def on_message(message):
@@ -13,7 +14,7 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
 @client.event
 async def on_ready():
@@ -22,6 +23,19 @@ async def on_ready():
     print(client.user.id)
     print(client.user.name+' is ready')
     print('------')
-    
+    botReady = True
+    return botReady
 
 client.run(TOKEN)
+
+channel = discord.utils.get(client.get_all_channels(), name='general')
+
+@client.event
+async def readyCheck(botReady):
+
+    if botReady == True:
+        readyMsg = client.user.name + 'is online!'
+        channel = client.get_channel(12324234183172)
+        await channel.send(readyMsg)
+        print ('botready = true')
+
