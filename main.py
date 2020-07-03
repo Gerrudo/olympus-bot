6c5fbf8267bb1a79c29c4eda50673f8a6004d624
Thomas Turner <thomasturner@pop-os.localdomain>
2020-07-03 17:21:43 +0100

    updated aysnc events

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
 import discord
 
-TOKEN = 'NzI3OTk2MTkwMDkxNzcyMDY3.Xvz-gQ.q6hxill_x1G_GvzF_ePo9F3jfig'
-
 client = discord.Client()
 
-botReady = False
+@client.event
+async def on_ready(client):
+    print('We have logged in as {0.user}'.format(client))
+
+    for server in client.servers:
+        if server.name == "Mini Chicken Kievs":
+            break
+    
+    for channel in server.channels:
+        if channel.name == "general":
+            break
+
+    botReady = True
+    if botReady == True:
+        await channel.send('Ready')
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await message.channel.send(msg)
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print(client.user.name+' is ready')
-    print('------')
-    botReady = True
-    return botReady
-
-client.run(TOKEN)
-
-channel = discord.utils.get(client.get_all_channels(), name='general')
-
-@client.event
-async def readyCheck(botReady):
-
-    if botReady == True:
-        readyMsg = client.user.name + 'is online!'
-        channel = client.get_channel(12324234183172)
-        await channel.send(readyMsg)
-        print ('botready = true')
-
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+client.run('NzI3OTk2MTkwMDkxNzcyMDY3.Xvz-gQ.q6hxill_x1G_GvzF_ePo9F3jfig')
